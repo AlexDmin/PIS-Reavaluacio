6425bb377c60feae8bb010d9c6c6e4f5f9dfc527
AlexDmin <32068464+AlexDmin@users.noreply.github.com>
2018-06-12 19:55:00 +0200

    Activities de topics, topic i new exam.

---
+++ app/src/main/java/com/example/alex/examgroups/NewExamActivity.java
@@ -0,0 +1,20 @@
+package com.example.alex.examgroups;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import Controller.Controller;
+
+/**
+ * Created by alex on 12/06/2018.
+ */
+
+public class NewExamActivity extends AppCompatActivity {
+    private Controller controller;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
